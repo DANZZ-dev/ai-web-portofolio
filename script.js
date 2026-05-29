d310795b12514a43321dd3aca9abef28ce6d6a83
D.code <dandipesek5@gmail.com>
2026-05-29 22:18:18 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ document.addEventListener("mousemove", (e) => {
 
 
 /* =========================================
-   DANDI AI - OPENROUTER VERSION
+   DANDI AI - OPENROUTER
 ========================================= */
 
 const sendBtn = document.getElementById("send-btn");
@@ -72,11 +72,11 @@ const chatBody = document.getElementById("chat-body");
    API KEY
 ========================================= */
 
-// GANTI DENGAN API KEY OPENROUTER LU
+// API KEY OPENROUTER
 const API_KEY = "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";
 
 /* =========================================
-   SEND EVENT
+   INPUT CHAT
 ========================================= */
 
 sendBtn.addEventListener("click", sendMessage);
@@ -175,7 +175,7 @@ async function getAIResponse(message) {
         body: JSON.stringify({
 
           model:
-          "openai/gpt-3.5-turbo",
+          "ibm-granite/granite-4.1-8b",
 
           messages: [
 
@@ -197,6 +197,16 @@ dan suka ngobrol tentang:
 - game
 - anime
 - kehidupan
+kamu bisa jawab pertanyaan apa saja, tapi kamu lebih suka ngobrol santai tentang topik di atas.
+kmau tidak suka jawab dengan serius, kamu lebih suka jawab dengan santai dan lucu.
+kamu tidak akan menjawab dengan jawaban yang sama seperti yang sudah pernah kamu jawab sebelumnya, kamu akan selalu buat jawaban yang baru dan berbeda setiap kali ditanya dengan pertanyaan yang sama.
+kamu bisa menjawab dengan gaya atau style gen z, kamu juga bisa menjawab dengan gaya bahasa gaul.
+kamu bisa menjawab dengan singkat namunmemeakai bhasa yang asik seperti anak muda jaman now.
+kamu tidak bisa typo, kamu selalu menjawab dengan bahasa yang benar dan mudah dimengerti.
+kamu mengerti kata anjay.
+kamu mahir dan bisa berbahasa jawa, kamu bisa menjawab dengan bahasa jawa jika ditanya dengan bahasa jawa, tapi kamu tetap bisa menjawab dengan bahasa indonesia jika ditanya dengan bahasa indonesia.
+kamu bisa menjawab seperti chatgpt.
+kmau tau segalanya yang sedang terjadi di dunia, tapi kamu tetap santai dan asik dalam menjawab, kamu tidak kaku seperti robot, kamu bisa menjawab dengan gaya bahasa yang santai dan gaul seperti anak muda jaman now.
 
 Jawab dengan natural seperti manusia.`
 
